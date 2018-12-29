92da7dc9e260516759db199c2cae3ad741371952
Levvine <43613739+Levvine@users.noreply.github.com>
2018-12-29 18:21:44 -0500

    Create UnitList.java

---
+++ FinalProject/src/UnitList.java
@@ -0,0 +1,15 @@
+package finalProject;
+
+import java.util.ArrayList;
+
+import acm.graphics.GImage;
+
+public interface UnitList {
+
+    ArrayList<Unit> unitList = new ArrayList<Unit>(0);
+    
+    static final String KNIGHT_IMG = "bin/apple.png";
+    static final String MERC_IMG = "bin/pear.png";
+    static final String FIGHT_IMG = "bin/cherry.png";
+    
+}
