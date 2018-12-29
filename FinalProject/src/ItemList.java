baee4c86e6875a09977135b24f29cc7520e3216b
Levvine <43613739+Levvine@users.noreply.github.com>
2018-12-29 18:21:28 -0500

    Create ItemList.java

---
+++ FinalProject/src/ItemList.java
@@ -0,0 +1,10 @@
+package finalProject;
+
+public interface ItemList {
+
+    static final int NULL = -1;
+    static final int BRONZE_LANCE = 0;
+    static final int BRONZE_SWORD = 1;
+    static final int BRONZE_AXE = 2;
+    
+}
