dc06cb51adf381a8beb2ddd3c74813393120e0f8
Levvine <43613739+Levvine@users.noreply.github.com>
2018-12-29 18:21:42 -0500

    Create UnitInventory.java

---
+++ FinalProject/src/UnitInventory.java
@@ -0,0 +1,8 @@
+package finalProject;
+
+public interface UnitInventory {
+
+    Item[] inventory = {new Item(Item.NULL), new Item(Item.NULL), new Item(Item.NULL), new Item(Item.NULL),
+                        new Item(Item.NULL)};
+    
+}
