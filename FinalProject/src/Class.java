214f6fdc423b91b14bad72d8ca9117d1db36a0c1
Levvine <43613739+Levvine@users.noreply.github.com>
2018-12-29 18:21:22 -0500

    Create Class.java

---
+++ FinalProject/src/Class.java
@@ -0,0 +1,13 @@
+package finalProject;
+
+public interface Class {
+        
+    final int NULL = -1;
+    final int KNIGHT = 0;
+    final int MERCENARY = 1;
+    final int FIGHTER = 2;
+    
+    void setClassID(int id);
+    int getClassID();
+    
+}
