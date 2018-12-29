e41a562b04f13a15ab5028c57f69151d5409915f
Levvine <43613739+Levvine@users.noreply.github.com>
2018-12-29 18:21:20 -0500

    Create Actions.java

---
+++ FinalProject/src/Actions.java
@@ -0,0 +1,10 @@
+package finalProject;
+
+public interface Actions {
+
+    final static int SELF = 9;
+    final static int MOVEMENT = 1;
+    final static int ATTACK = 2;
+    final static int HEAL = 3;
+    
+}
